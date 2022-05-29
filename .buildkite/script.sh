db9a504b29f0f27312165efc8f6f79e4ac6e5a80
unknown <joeffrey.chaucer.paitan@gmail.com>
2022-05-29 23:47:21 +0800

    hello world

--- .buildkite/script.sh
+++ .buildkite/script.sh
@@ -8,7 +8,7 @@ echo $BUILDKITE_COMMIT
 git diff --stat HEAD
 
 
-if ! git diff --name-only HEAD | grep -qvE '(.md)'
+if ! git diff --name-only HEAD~ | grep -qvE '(.md)'
 then
     echo "Only doc files were updated, not running the CI."
     exit
