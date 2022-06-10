20fda77ff7e985699556af374d90aeed6db63235
Joeffrey Chaucer C. Paitan <joeffrey.chaucer.paitan@gmail.com>
2022-06-10 10:24:43 +0800

    Update script.sh

--- .buildkite/script.sh
+++ .buildkite/script.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-echo "$BUILDKITE_COMMIT"
 echo "$BUILDKITE_PULL_REQUEST_BASE_BRANCH"
 
 git diff --stat $BUILDKITE_PULL_REQUEST_BASE_BRANCH..$BUILDKITE_COMMIT
 
-if ! git diff --name-only $BUILDKITE_PULL_REQUEST_BASE_BRANCH..$BUILDKITE_COMMIT | grep -qvE '(.md)'; then
+if ! git diff --name-only $BUILDKITE_PULL_REQUEST_BASE_BRANCH.. | grep -qvE '(.md)'; then
     echo "Only doc files were updated, not running the CI."
     exit 1
 fi
