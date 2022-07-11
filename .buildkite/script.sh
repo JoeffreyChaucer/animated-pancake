bf4d62ab064872a436953d638152e0d991612533
Joeffrey <joeffrey.chaucer.paitan@gmail.com>
2022-07-11 18:31:17 +0800

    yrsy

--- .buildkite/script.sh
+++ .buildkite/script.sh
@@ -11,18 +11,28 @@ authenticate_to_sandbox() {
 }
 
 tempoary_file(){
+    echo "--- Getting to $1"
     local branch
     local version
     branch=$(buildkite-agent meta-data get "branch-release-stream")
     version=$(buildkite-agent meta-data get "release-version")
     git fetch
-    git show origin/$branch:.buildkite/release-docs-$version.sh > release-docs-$version.sh
+    git show origin/"$branch":.buildkite/release-docs-"$version".sh > release-docs-"$version".sh
+
 }
 
 
-deploy_elease_to_sandbox(){
+deploy_release_to_sandbox(){
+    local version
     echo "--- Release to $1"
+    version=$(buildkite-agent meta-data get "release-version")
     echo "release-docs-$version"
+}
+
+cleanup_temporary_file() {
+    local version
+    version=$(buildkite-agent meta-data get "release-version")
+    trap 'release-docs-$version' EXIT
 }
 
 
@@ -30,9 +40,8 @@ main() {
     install_sfdx_plugins
     authenticate_to_sandbox "$1"
     tempoary_file
-    deploy_elease_to_sandbox "$1"
-    trap 'release-docs-$version' EXIT
-
+    deploy_release_to_sandbox "$1"
+    cleanup_temporary_file
 }
 
 main "$1"
